7da75208d5faf8c960f0dfbec190f3e8e3fdda94
Daulton Barbour <daultonbarbour@gmail.com>
2025-03-31 16:53:14 -0700

    Added .py file that prints the Hagrid quote

---
+++ S25_Barbour.py
@@ -0,0 +1,3 @@
+print(f"What's coming will come and we'll meet it when it does\n"
+      "                 - Hagrid                             \n"
+      "                         - J.K. Rowling, Harry Potter and the Goblet of Fire (2000)")
